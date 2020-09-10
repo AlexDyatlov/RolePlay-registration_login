d1facf7673abe575ee3a3afc32c93545342178c5
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-09-10 21:31:02 +0300

    added validation form

--- src/js/main.js
+++ src/js/main.js
@@ -11,5 +11,34 @@ $(function(){
 
   });
 
+  var checker = document.getElementById('checkbox');
+  var sendbtn = document.getElementById('send-sms');
+
+    checker.onchange = function(){
+
+    if(this.checked){
+      sendbtn.disabled = false;
+    } 
+
+    else{
+      sendbtn.disabled = true;
+    }
+  };
+  
+  $("#passsecond").keyup(validate);
+  
+  function validate() {
+    var password1 = $("#passfirst").val();
+    var password2 = $("#passsecond").val();
+   
+      if(password1 == password2) {
+         $("#validate-status").html('<img src="images/icons/validation.png" alt="">');
+      }
+      else {
+          $("#validate-status").html('');
+      }
+      
+  }
+
 });
 
